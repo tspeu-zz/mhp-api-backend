fc58463729992375ecb1db0cf97eac1d7658b89c
jm_b <jmbanchero@gmail.coom>
2019-09-30 00:27:33 +0100

    fallo geo point

--- models/parking.model.js
+++ models/parking.model.js
@@ -5,10 +5,7 @@ const ParkingSchema = mongoose.Schema(
     idUser: Number,
     text: String,
     idParking: Number,
-    coordinates: {
-      type: [Number],
-      index: '2dsphere'
-    },
+    location: GeoJSON.Point,
 
     isEnterParking: Boolean
   },
@@ -16,9 +13,13 @@ const ParkingSchema = mongoose.Schema(
     timestamps: true
   }
 );
+GeoJSON.Point = new mongoose.Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: [{ type: 'Number' }]
+});
 /*The coordinates will be stored as an array in the format 
 [longitude, latitude]. We index on location and let mongoDB know we are using a “2dsphere”. */
-//ParkingSchema.index({ location: '2dsphere' });
+ParkingSchema.index({ location: '2dsphere' });
 // ParkingSchema.index({ location: '2d' });
 /* 
 location: {
