129716c18b34bae9b3ba32cf773bfa043fb5e3d4
jm_b <jmbanchero@gmail.coom>
2019-09-30 23:05:08 +0100

    add CORS

--- routes/parking.routes.js
+++ routes/parking.routes.js
@@ -1,20 +1,37 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const parking = require('../controllers/parking.controller');
-
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100'
+];
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+};
 // Retrieve all menus
-router.get('/', parking.findAll);
+router.get('/', cors(corsOptions), parking.findAll);
 
 // Retrieve a single menu with menuId
-router.get('/:idUser', parking.findOne);
+router.get('/:idUser', cors(corsOptions), parking.findOne);
 
 // Create a new Menu
-router.post('/', parking.create);
+router.post('/', cors(corsOptions), parking.create);
 
 // Update a Note with menuId
-router.put('/:idUser', parking.update);
+router.put('/:idUser', cors(corsOptions), parking.update);
 
 // Delete a Note with menuId
-router.delete('/:idUser', parking.delete);
+router.delete('/:idUser', cors(corsOptions), parking.delete);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
+const cors = require('cors');
+
 const serverPort = config.app.port;
 const {
   db: { host, port, name }
 } = config;
 const connectionString = `mongodb://${host}:${port}/${name}`;
-
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100'
+];
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+};
 // Get the API routes
 const router = express.Router();
 // Get the API route ...
